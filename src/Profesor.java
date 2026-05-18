65c355996e7b245a1745593a492db786f39d94c5
Lucero Franco Emiliano <129550333+LuceroFrancoEmiliano@users.noreply.github.com>
2026-05-18 19:32:49 -0300

    Update Profesor.java

--- src/Profesor.java
+++ src/Profesor.java
@@ -1,52 +1,44 @@
 public class Profesor extends Persona implements MiembroUniversidad {
     private String especialidad;
     private int añosDeExperiencia;
-    private int materiasAsignadas;
     private Materia[] materias;
+    private int contadorMaterias; 
 
     public Profesor(String nombre, String apellido, int edad, String documento, String especialidad, int añosDeExperiencia) {
         super(nombre, apellido, edad, documento);
         this.especialidad = especialidad;
         this.añosDeExperiencia = añosDeExperiencia;
-        this.materiasAsignadas = 0;
-        this.materias = new Materia[10]; 
+        this.contadorMaterias = 0;
+        this.materias = new Materia[10];
     }
 
-    public String getEspecialidad() {
-        return especialidad;
-    }
-    public void setEspecialidad(String especialidad) {
-        this.especialidad = especialidad;
-    }
+    public String getEspecialidad() { return especialidad; }
+    public void setEspecialidad(String especialidad) { this.especialidad = especialidad; }
 
-    public int getAñosDeExperiencia() {
-        return añosDeExperiencia;
-    }
+    public int getAñosDeExperiencia() { return añosDeExperiencia; }
     public void setAñosDeExperiencia(int añosDeExperiencia) {
-        if (añosDeExperiencia >= 0) {
-            this.añosDeExperiencia = añosDeExperiencia;
-        }
+        if (añosDeExperiencia >= 0) this.añosDeExperiencia = añosDeExperiencia;
     }
-    public int getMateriasAsignadas() { 
-        return materiasAsignadas;
-    }
+
+    public int getContadorMaterias() { return contadorMaterias; }
+
     public void asignarMateria(Materia materia) {
-        if (materiasAsignadas < materias.length) {
-            this.materias[materiasAsignadas] = materia;
-            materiasAsignadas++;
+        if (contadorMaterias < materias.length) {
+            this.materias[contadorMaterias] = materia;
+            contadorMaterias++;
         } else {
-            System.out.println("Limite de materias asignadas alcanzado.");
+            System.out.println("Límite de materias asignadas alcanzado.");
         }
     }
 
+    @Override
     public String toString() {
         return "Profesor: " + getNombre() + " " + getApellido() + ", Especialidad: " + especialidad + ", Años de Experiencia: " + añosDeExperiencia;
     }
-    
-    public String obtenerRol() {
-        return "Profesor";
-    }
-    public String obtenerInformacionCompleta() {
-        return toString();
-    }
+
+    @Override
+    public String obtenerRol() { return "Profesor"; }
+
+    @Override
+    public String obtenerInformacionCompleta() { return toString(); }
 }
